3e1a727f10c2e0fbb142291c03d9b2501f827846
Jens Rosseel <jens.rosseel2@student.hogent.be>
2025-06-25 11:51:29 +0200

    add user & userList component + install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
 
 const App = () => {
-
   return (
     <div>
-      <h1>Overview</h1>
+      <h1 className="text-3xl">Overview</h1>
       <p>Here you can find user details.</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     watch: {
-      ignored: ['**/server/db.json'],
+      ignored: ["**/server/db.json"],
     },
   },
-})
+});
