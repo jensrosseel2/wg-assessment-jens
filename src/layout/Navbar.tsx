2c8fc78b6489fa02d0f3cfd620c298dc39cde436
Jens Rosseel <jens.rosseel2@student.hogent.be>
2025-06-26 17:57:44 +0200

    add loading indicator

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,10 +1,13 @@
 import logo from "../assets/wegroup-logo-full.svg";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="bg-white w-full top-0 start-0 border-b border-gray-200 shadow-lg mb-3">
       <div className="flex justify-center items-center w-full my-2">
-        <img src={logo} alt="" className="h-15" />
+        <Link to={"/"}>
+          <img src={logo} alt="" className="h-15" />
+        </Link>
       </div>
     </nav>
   );
